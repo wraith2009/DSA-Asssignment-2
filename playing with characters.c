061f1a244f98acdf272ffc67c6a638b270a8b0fe
rahul bhardwaj <113882096+wraith2009@users.noreply.github.com>
2022-09-25 19:55:47 +0530

    playing with characters.c

---
+++ playing with characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c,s[50],sen[100];
+    scanf("%c",&c);
+    scanf("%s\n",&s);
+    gets(sen);
+    
+    
+    printf("%c",c);
+    printf("\n%s",s);
+    printf("\n%s",sen);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
